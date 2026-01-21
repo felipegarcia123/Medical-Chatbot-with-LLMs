a78b9aae40650ab8ae26582838c3d75f266d6314
Juan Felipe Garcia <juangarciajuanico@hotmail.com>
2026-01-21 17:40:31 -0500

    requirements and setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Felipe Garcia",
+    author_email="juanga@hotm.com",
+    description="A Medical Chatbot using LLMs, LangChain, Pinecone, Flask, and AWS",
+    packages=find_packages(),
+    install_requires=[]
+) 
